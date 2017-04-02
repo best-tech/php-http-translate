ef75c06f17b20f05a7344e781c85901fcda39b5b
Denis Kharchenko <kharchenkodenis@yandex.ru>
2017-04-02 08:22:04 +0300

    first

---
+++ readme.md
@@ -0,0 +1,6 @@
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/best-tech/php-http-translate/tree/master)
+
+# Ретранслятор HTTP GET и POST запросов
+
+## Когда нужен https а его нет
+
---
+++ web/index.php
@@ -0,0 +1,26 @@
+<?php
+require('../vendor/autoload.php');
+
+ini_set('display_errors',1);
+ini_set("error_reporting", E_ALL);
+
+$REQUEST_METHOD = $_SERVER['REQUEST_METHOD'];
+
+$DEST = getenv('DEST');
+
+
+if ($REQUEST_METHOD=='POST')
+{
+	
+	
+	
+}
+elseif ($REQUEST_METHOD=='GET')
+{
+	
+	
+	
+}
+
+
+?>
