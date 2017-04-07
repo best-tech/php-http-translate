3ec238b462851b42775031a94299df5ec743fdc4
Denis Kharchenko <kharchenkodenis@yandex.ru>
2017-04-07 17:56:53 +0300

    new

--- web/index.php
+++ web/index.php
@@ -14,53 +14,85 @@ $DEST_FINAL = $DEST.$uri;
 
 if ($REQUEST_METHOD=='POST')
 {
-	$baseText = (string) implode("", file('php://input'));
-    $contLenght = strlen($baseText);
-	// $requestHeaders = array(
-    //     'Content-Type: application/json',
-    //     sprintf('Content-Length: %d',  $contLenght)
-    //     );
-	// 	Создать контекст и инициализировать POST запрос
-	 $context = stream_context_create(array(
-	        'http' => array(
-	            'method' => $REQUEST_METHOD,
-	            //'header' => $requestHeaders,
-	            'content' => $baseText,
-	        ),
-	    ));
-	
-		try{
-			
-			echo file_get_contents($file = $DEST_FINAL,$use_include_path = false,$context);
-		}
-	   	 
-		catch (Exception $e) {
-    		echo 'off try';
-			}
+    $baseText = (string) implode("", file('php://input'));
+
+    $headers = getallheaders();
+
+//    unset($headers['Host']);
+//    unset($headers['Accept-Language']);
+
+//    $headers["Content-Type"] = "application/json"
+//    $headers["Content-Length"] = strlen($baseText);
+
+    $requestHeaders = [];
+    foreach($headers as $key => $value) {
+        $requestHeaders[] = $key . ": " . $value;
+    }
+
+//    $requestHeaders[] = "Content-Type: application/json";
+
+
+
+
+//     $requestHeaders = array(
+//         'Content-Type: application/json',
+//            sprintf('Content-Length: %d',  $contLenght)
+//         );
+//                Создать контекст и инициализировать POST запрос
+    $context = stream_context_create(array(
+        'http' => array(
+            'method' => $REQUEST_METHOD,
+            'header' => $requestHeaders,
+            'content' => $baseText,
+        ),
+    ));
+
+    try{
+
+        echo file_get_contents($DEST_FINAL,false,$context);
+    }
+
+    catch (Exception $e) {
+        echo 'off try';
+    }
 }
 else
 {
-	$baseText = '';
-	
-	//e	cho $DEST_FINAL;
-	// 	Создать контекст и инициализировать POST запрос
-	 $context = stream_context_create(array(
-	        'http' => array(
-	            'method' => $REQUEST_METHOD,
-	            'header' => 'Content-Type: application/json' . PHP_EOL,
-	            'content' => $baseText,
-	        ),
-	    ));
-	
-	// 	Отправить запрос на себя, чтобы запустить тесты
-	    // 	и показать результат выполнения тестов
-	    echo file_get_contents(
-	        $file = $DEST_FINAL,
-	        $use_include_path = false,
-	        $context);
-	
+    $baseText = '';
+
+    //e         cho $DEST_FINAL;
+    //            Создать контекст и инициализировать POST запрос
+    $context = stream_context_create(array(
+        'http' => array(
+            'method' => $REQUEST_METHOD,
+            'header' => 'Content-Type: application/json' . PHP_EOL,
+            'content' => $baseText,
+        ),
+    ));
+
+    //            Отправить запрос на себя, чтобы запустить тесты
+    //        и показать результат выполнения тестов
+    echo file_get_contents(
+        $file = $DEST_FINAL,
+        $use_include_path = false,
+        $context);
+
 }
 
 
+
+
+function getallheaders()
+{
+    $headers = '';
+    foreach ($_SERVER as $name => $value)
+    {
+        if (substr($name, 0, 5) == 'HTTP_')
+        {
+            $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+        }
+    }
+    return $headers;
+}
 
 ?>
